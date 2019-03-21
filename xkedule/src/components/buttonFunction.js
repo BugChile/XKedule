75dceee4e4d42c074c996a24f14ce7478628d103
Javier Bahamondes <jfbahamondes@uc.cl>
2019-03-21 11:35:32 -0300

    buttons event info

--- xkedule/src/components/buttonFunction.js
+++ xkedule/src/components/buttonFunction.js
@@ -3,7 +3,9 @@ import React from "react"
 export default class ButtonFunction extends React.Component {
     render() {
         return (
-         <span className='button_min' onClick={this.props.function}>{this.props.text}</span>   
+         <span className={'button_min '.concat(this.props.cssClass)} onClick={this.props.function}>
+         <i className={"glyphicon glyphicon-".concat(this.props.cssIcon)}>{this.props.text}</i>
+         </span>   
         )
   }
 }
--- xkedule/src/components/content_layouts/infoCard.js
+++ xkedule/src/components/content_layouts/infoCard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ButtonFunction from '../buttonFunction'
+
 export default class InfoCard extends React.Component {
     render() {
         return (
@@ -8,8 +9,24 @@ export default class InfoCard extends React.Component {
           case null:   return (<div>asd</div>) ;
           default:  return (
             <div value='1' className={''.concat(this.props.classesInfoCard).concat(' ').concat(this.props.topValue)}>
-            <ButtonFunction function={this.props.functionClose} text='X'/>
-            {/* <ButtonFunction function={this.props.functionClose} text='X'/> */}
+            <div className="button-container">
+            <ButtonFunction function={this.props.functionClose} cssClass="trash" cssIcon="glyphicon glyphicon-trash"/>
+
+            <ButtonFunction function={this.props.functionClose} cssClass="exit" cssIcon="glyphicon glyphicon-remove"/>
+            {
+              (
+                (() => {
+             switch (this.props.event.link) {
+              case null: case "":   case false: case undefined:   return (<div></div>) ;
+             default:  return (
+            <ButtonFunction function={this.props.functionClose} cssClass="link" cssIcon="glyphicon glyphicon-link"/>
+             );
+             }
+         })())
+            }
+            <ButtonFunction function={this.props.functionClose} cssClass="edit" cssIcon="glyphicon glyphicon-edit"/>
+
+            </div>
             <h3>Event: {this.props.event.title}</h3>
             <h5>{this.props.event.date_start.toDateString()},</h5>
             <h4> {this.props.event.date_start.toLocaleTimeString('en-GB', {hour: "2-digit", minute: "2-digit"})}<span>  -  </span> {this.props.event.date_end.toLocaleTimeString('en-GB', {hour: "2-digit", minute: "2-digit"})}</h4>
