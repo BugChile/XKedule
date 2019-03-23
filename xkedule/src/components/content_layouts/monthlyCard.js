ef5bb6b1938ab1430a929a4794dad43c7930052b
DiegoAndai <dfandai@uc.cl>
2019-03-23 17:10:00 -0300

    Monthly view see more ready

--- xkedule/src/components/content_layouts/monthlyCard.js
+++ xkedule/src/components/content_layouts/monthlyCard.js
@@ -3,28 +3,37 @@ import HeaderDate from './headerDate'
 import MonthlyCardCell from './monthlyCardCell'
 import MonthlyTaskCard from './monthlyTaskCard'
 import { get_day_occurrence } from '../../js_helpers/rrule_helpers'
+import { dateToWritenDate } from '../../js_helpers/parsers'
 
 
 export default class MonthlyCard extends React.Component {
     constructor(props){
         super(props)
+
         this.state = {
+            tasks_per_cell: this.calculateMaxTasks(),
             expanded_tasks: [],
             expanded_class: "expanded_month_cell hidden",
             expanded_top: 0,
             expanded_left: 0,
+            expanded_day: null,
         }
 
         this.generateTaskCards = this.generateTaskCards.bind(this);
         this.generateDayCells = this.generateDayCells.bind(this);
         this.getHeaderDate = this.getHeaderDate.bind(this);
         this.seeMore = this.seeMore.bind(this);
+        this.closeSeeMore = this.closeSeeMore.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.calculateMaxTasks = this.calculateMaxTasks.bind(this);
+        this.onResize = this.onResize.bind(this);
     };
 
     generateTaskCards(day, events, tasks_per_cell, day_id){
         // day is a string dd/mm/yyyy
         var task_cards = []
-        const day_events = events[day];
+        const day_string = day.toLocaleDateString();
+        const day_events = events[day_string];
         if (day_events) {
             day_events.forEach((event) => {
                     task_cards.push(
@@ -35,8 +44,8 @@ export default class MonthlyCard extends React.Component {
             if (tasks_per_cell && task_cards.length > tasks_per_cell) { // add "see more"
                 const not_shown_count = task_cards.length - tasks_per_cell + 1; // +1 because see more uses 1 space
                 task_cards = task_cards.slice(0, tasks_per_cell - 1);
-                task_cards.push(<MonthlyTaskCard event={{id: "see_more_"+day,
-                                                         day: day,
+                task_cards.push(<MonthlyTaskCard event={{id: "see_more_"+day_string,
+                                                         day: new Date(day),
                                                          title: `See ${not_shown_count} more`}}
                                                  clickEvent={(card, id) =>
                                                                     {this.seeMore(card,
@@ -51,18 +60,21 @@ export default class MonthlyCard extends React.Component {
         return task_cards
     }
 
-
-
-    generateDayCells(current_time, events){
-        var day_cells = [];
-        const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-        var day_date = new Date(current_time.getFullYear(), current_time.getMonth(), 1); //first day of month
-        const next_month = current_time.getMonth() === 11 ? 0 : current_time.getMonth() + 1;
-        const week_count = get_day_occurrence(new Date(current_time.getFullYear(),
+    calculateMaxTasks(){
+        const next_month = this.props.current_time.getMonth() === 11 ? 0 : this.props.current_time.getMonth() + 1;
+        const week_count = get_day_occurrence(new Date(this.props.current_time.getFullYear(),
                                                        next_month,
                                                        0)); //number of weeks on this month
         const cell_height = (document.getElementById("content").clientHeight / week_count) - 30;
-        const tasks_per_cell = Math.floor(cell_height / 20);
+        return Math.floor(cell_height / 20);
+    }
+
+
+
+    generateDayCells(current_time, events, tasks_per_cell){
+        var day_cells = [];
+        const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+        var day_date = new Date(current_time.getFullYear(), current_time.getMonth(), 1); //first day of month
 
         day_date.setDate(day_date.getDate() - (day_date.getDay() + 6) % 7) //first month's week monday date
 
@@ -93,7 +105,7 @@ export default class MonthlyCard extends React.Component {
                           </div>)
 
             day_id = "monthly_cell"+day_date.getTime()
-            day_tasks = this.generateTaskCards(day_date.toLocaleDateString(),
+            day_tasks = this.generateTaskCards(day_date,
                                                events,
                                                tasks_per_cell,
                                                day_id);
@@ -121,13 +133,26 @@ export default class MonthlyCard extends React.Component {
 
     seeMore(see_more_card, events, day_id){
         const expanded_tasks = this.generateTaskCards(see_more_card.day, events);
-        const expanded_class = "expanded_month_cell"
+        const expanded_day = dateToWritenDate(see_more_card.day);
+        const expanded_class = "expanded_month_cell visible"
         const expanded = document.getElementById("monthly_cell_expanded");
-        const day_cell = document.getElementById(day_id);
-        console.log(day_cell, day_cell.getBoundingClientRect())
-        expanded.style.top = day_cell.getBoundingClientRect().top + "px";
-        expanded.style.left = day_cell.getBoundingClientRect().left + 2 + "px";
-        this.setState({expanded_tasks, expanded_class: "expanded_month_cell"});
+        const day_cell_props = document.getElementById(day_id).getBoundingClientRect();
+        const content_props = document.getElementById("content").getBoundingClientRect();
+
+        expanded.style.top = Math.min(day_cell_props.top - 10,
+                                      content_props.height + 30 - 22 * expanded_tasks.length) + "px";
+        expanded.style.left = Math.max(day_cell_props.left - 4, 30) + "px";
+        expanded.style.width = day_cell_props.width + 12 + "px";
+        expanded.style.opacity = 1;
+
+        this.setState({expanded_tasks, expanded_class, expanded_day});
+
+    }
+
+    closeSeeMore(see_more_card, events, day_id){
+        const expanded_tasks = [];
+        const expanded_class = "expanded_month_cell hidden"
+        this.setState({expanded_tasks, expanded_class});
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -140,6 +165,18 @@ export default class MonthlyCard extends React.Component {
       } else {
         return true;
       }
+  }
+
+  onResize(){
+      const tasks_per_cell = this.calculateMaxTasks();
+      if (tasks_per_cell !== this.state.tasks_per_cell) {
+          this.setState({tasks_per_cell});
+          this.forceUpdate();
+      }
+  }
+
+    componentDidMount(){
+        window.addEventListener("resize", this.onResize);
     }
 
 
@@ -154,15 +191,19 @@ export default class MonthlyCard extends React.Component {
             </div>
             <div className="content" id="content">
 
-                {<MonthlyCardCell day_tasks={this.state.expanded_tasks}
-                                  cell_key={"monthly_cell_expanded"}
-                                  day_info={"expanded"}
-                                  cell_class_list={this.state.expanded_class}/>}
+
                  <div className="monthly_schedule">
                      {this.generateDayCells(this.props.current_time,
-                                            this.props.events)}
+                                            this.props.events,
+                                            this.state.tasks_per_cell)}
 
                  </div>
+
+                 {<MonthlyCardCell day_tasks={this.state.expanded_tasks}
+                                   cell_key={"monthly_cell_expanded"}
+                                   day_info={this.state.expanded_day}
+                                   cell_class_list={this.state.expanded_class}
+                                   closeEvent={this.closeSeeMore}/>}
 
 
             </div>
--- xkedule/src/components/content_layouts/monthlyCardCell.js
+++ xkedule/src/components/content_layouts/monthlyCardCell.js
@@ -3,11 +3,14 @@ import React from "react"
 export default class MonthlyCardCell extends React.Component {
 
   render() {
+      const close_div = <i className='fas fa-times' onClick={this.props.closeEvent}></i>
+
       return(
           <div
           className={this.props.cell_class_list}
           key={this.props.cell_key}
           id={this.props.cell_key}>
+                  {this.props.closeEvent ? close_div : null}
                    <div className="day_cell_info">
                        {this.props.day_info}
                    </div>
